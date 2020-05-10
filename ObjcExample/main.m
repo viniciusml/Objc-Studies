29de98149e802a8ebd3c40db71907f20a824810a
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-05-10 17:35:41 +0200

    Basic definitions

--- ObjcExample/main.m
+++ ObjcExample/main.m
@@ -6,6 +6,11 @@
 //  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
 //
 
+// Any line of code that begins with # is a preprocessor directive. It finds the header file for Foundation (Apple’s fundamental Objective-C framework), and paste it here.
+// @ - means this next bit is Objective-C, and definitely not C.
+// @autoreleasepool - it's going to be allocating lots of memory; when finished, please free it up
+// Import my own header files: #import "MyClass.h"
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
