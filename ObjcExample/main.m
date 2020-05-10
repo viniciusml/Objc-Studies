c6cb3eb3de331672f22e52c154401ad0043004c2
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-05-10 17:27:24 +0200

    Initial Commit

---
+++ ObjcExample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjcExample
+//
+//  Created by Vinicius Moreira Leal on 10/05/2020.
+//  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
